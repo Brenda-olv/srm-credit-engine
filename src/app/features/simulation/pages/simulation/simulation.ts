948f258568931e9fe3dbd95f55776fde02ff1e13
Brenda-olv <marilisebrenda@hotmail.com>
2026-05-29 18:13:59 -0300

    feat: implementacao simulation reactive form

--- src/app/features/simulation/pages/simulation/simulation.ts
+++ src/app/features/simulation/pages/simulation/simulation.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule
+} from '@angular/forms';
 
 @Component({
   selector: 'app-simulation',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './simulation.html',
   styleUrl: './simulation.scss',
 })
-export class SimulationComponent { }
+export class SimulationComponent {
+
+
+  simulationForm = new FormGroup({
+    valorRecebivel: new FormControl<number>(100000),
+    prazo: new FormControl<number>(12),
+    tipoRecebivel: new FormControl<string>('duplicata'),
+    moedaTitulo: new FormControl<string>('BRL'),
+    moedaLiquidacao: new FormControl<string>('BRL')
+  });
+
+  constructor() {
+    this.simulationForm.valueChanges.subscribe(value => {
+      console.log(value);
+    });
+  }
+}
